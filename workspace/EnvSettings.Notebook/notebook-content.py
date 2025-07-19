e1b197389126c4c86555118236379b6ed929189c
Ernst Wolf <wolf@dejongonline.onmicrosoft.com>
2025-07-19 20:45:31 +0200

    replaced ids 1

--- workspace/EnvSettings.Notebook/notebook-content.py
+++ workspace/EnvSettings.Notebook/notebook-content.py
@@ -17,7 +17,7 @@
 # CELL ********************
 
 # Fabric Workspace ID for the Bronze medallion layer
-bronzeWorkspaceId = "8d8d00a7-0e8a-4e3b-8c0e-8dcafac7adec"
+bronzeWorkspaceId = "ac8278dd-5e74-4219-a61f-c6d1989495dc"
 
 # Bronze Lakehouse name. Set to None if not applicable.
 bronzeLakehouseName = "lh_bronze"
@@ -26,7 +26,7 @@ bronzeLakehouseName = "lh_bronze"
 bronzeDatawarehouseName = None
 
 # Fabric Workspace ID for the Silver medallion layer. Use the same ID if all medallion layers are in the same workspace.
-silverWorkspaceId = "8d8d00a7-0e8a-4e3b-8c0e-8dcafac7adec"
+silverWorkspaceId = "ac8278dd-5e74-4219-a61f-c6d1989495dc"
 
 # Silver Lakehouse name. Set to None if not applicable.
 silverLakehouseName = "lh_silver"
@@ -35,7 +35,7 @@ silverLakehouseName = "lh_silver"
 silverDatawarehouseName = None
 
 # Fabric Workspace ID for the Gold medallion layer. Use the same ID if all medallion layers are in the same workspace.
-goldWorkspaceId = "8d8d00a7-0e8a-4e3b-8c0e-8dcafac7adec"
+goldWorkspaceId = "ac8278dd-5e74-4219-a61f-c6d1989495dc"
 
 # Gold Lakehouse name. Set to None if not applicable.
 goldLakehouseName = None
